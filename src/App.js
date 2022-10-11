336c5546def21cfc0f4cbc15466b4547f5094a67
Yuriy <ykantakuzin@bk.ru>
2022-10-11 14:01:01 +0300

    added a job card reducer and a filtering reducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import { FilterPanel } from 'components/FilterPanel';
 import { JobList } from 'components/JobList';
 import { TheHeader } from 'components/TheHeader';
 
+import { addPositions } from 'store/positions/positions-actions';
+import data from './mock/data.json';
+
 function App() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(addPositions(data));
+	}, [dispatch]);
+
+
 	return (
 		<>
 			<TheHeader />
--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -4,19 +4,19 @@ import { Stack } from 'UI/Stack';
 
 
 const FilterPanel = () => {
-  return (
-    <Card className="filter-panel">
-      <div className="filter-panel-wrapper">
-        <Stack>
-          <Badge variant="clearable">Frontend</Badge>
-          <Badge variant="clearable">Backend</Badge>
-          <Badge variant="clearable">React</Badge>
-        </Stack>
+	return (
+		<Card className="filter-panel">
+			<div className="filter-panel-wrapper">
+				<Stack>
+					<Badge variant="clearable">Frontend</Badge>
+					<Badge variant="clearable">Backend</Badge>
+					<Badge variant="clearable">React</Badge>
+				</Stack>
 
-        <button className='link'>Clear</button>
-      </div>
-    </Card>
-  )
+				<button className='link'>Clear</button>
+			</div>
+		</Card>
+	)
 }
 
-export {FilterPanel};
+export { FilterPanel };
